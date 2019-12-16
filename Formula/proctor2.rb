62bf7cef7d2d7ceacb0f5964ca517d2a470084fd
Deny Prasetyo <jasoet87@gmail.com>
2019-12-16 11:38:54 +0700

    [Jasoet] fix(proctor): Release Version to v2.0.3

--- Formula/proctor2.rb
+++ Formula/proctor2.rb
@@ -1,7 +1,7 @@
 class Proctor2 < Formula
   desc "Proctor v2 CLI"
   homepage "https://github.com/gopaytech/proctor"
-  url "https://github.com/gopaytech/proctor/releases/download/v2.0.2/proctor_2.0.3_Darwin_x86_64.tar.gz"
+  url "https://github.com/gopaytech/proctor/releases/download/v2.0.3/proctor_2.0.3_Darwin_x86_64.tar.gz"
   version "2.0.3"
   sha256 "66548c5f4e53e7de4fe73473fa163bd1037e35df21bdc84e30a75eb2d57b8762"
   head "https://github.com/gopaytech/proctor.git"
