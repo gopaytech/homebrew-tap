459958387d0fbaf0e94af5540962d4500d6718e5
William Albertus Dembo <w.albertusd@gmail.com>
2020-01-13 11:15:34 +0700

    feat(proctor): Release version v2.0.4

--- Formula/proctor2.rb
+++ Formula/proctor2.rb
@@ -1,9 +1,9 @@
 class Proctor2 < Formula
   desc "Proctor v2 CLI"
   homepage "https://github.com/gopaytech/proctor"
-  url "https://github.com/gopaytech/proctor/releases/download/v2.0.3/proctor_2.0.3_Darwin_x86_64.tar.gz"
-  version "2.0.3"
-  sha256 "66548c5f4e53e7de4fe73473fa163bd1037e35df21bdc84e30a75eb2d57b8762"
+  url "https://github.com/gopaytech/proctor/releases/download/v2.0.4/proctor_2.0.4_Darwin_x86_64.tar.gz"
+  version "2.0.4"
+  sha256 "d3650849af45441761d3b7261d2027e89febcd6a7805d2a493dcac9a86a6d95e"
   head "https://github.com/gopaytech/proctor.git"
 
   bottle :unneeded
