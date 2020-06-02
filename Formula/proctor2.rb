e7462aab9b47591c68f62217b18ad02a21715bf6
raymond.ralibi <raymond.ralibi@go-jek.com>
2020-06-02 14:27:04 +0700

    feat(proctor): Fixed sha

--- Formula/proctor2.rb
+++ Formula/proctor2.rb
@@ -3,7 +3,7 @@ class Proctor2 < Formula
   homepage "https://github.com/gopaytech/proctor"
   url "https://github.com/gopaytech/proctor/releases/download/v2.0.8/proctor_2.0.8_Darwin_x86_64.tar.gz"
   version "2.0.8"
-  sha256 "75e6a5137397da704716c18bb4513d19cda6ef07fdb0ebd8512e8b63d15bbbde"
+  sha256 "1e0d6e3f1c27bc2f959bd9ee7c34ad41110085e818ae2ac2e66c91f2718007f6"
   head "https://github.com/gopaytech/proctor.git"
 
   bottle :unneeded
